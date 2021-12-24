628221e993584b7ccface7f4422f87cde7cf6508
AkilaMani <akilamani2@gmail.com>
2021-12-24 16:40:03 +0530

    QrCode Scanner

---
+++ lib/values/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color orangeAccent = Colors.orangeAccent;
+  static const Color blueAccent = Colors.blueAccent;
+}
---
+++ lib/values/strings.dart
@@ -0,0 +1,12 @@
+class Strings{
+  static const String title ='QrCode Scanner';
+  static const String qrView ='QrCode View';
+  static const String resume ='Resume';
+  static const String pause ='Pause';
+  static const String flash ='Flash: ';
+  static const String scanCode ='Scan a code';
+  static const String barCodeType ='Barcode Type: ';
+  static const String data ='Data: ';
+  static const String cameraFacing ='Camera Facing: ';
+  static const String loading ='loading';
+}
